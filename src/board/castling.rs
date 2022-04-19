a132f60ba7809c6e2a898c552dbcfbe9dcee4bbc
mattngaw <mattngaw2002@gmail.com>
2022-04-19 00:23:14 -0400

    Changed castling interface to use get set reset

--- src/board/castling.rs
+++ src/board/castling.rs
@@ -1,8 +1,6 @@
 //! Keeps track of castling rights
 
 use super::*;
-use std::ops;
-use ux::*;
 
 /// Represents the two sides where one can castle
 #[derive(Debug, PartialEq)]
@@ -10,52 +8,85 @@ pub enum CastlingSide { Kingside, Queenside }
 
 /// A 4-bit word representing who still has castling rights
 #[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Castling(u4);
+pub struct Castling(u8);
+
+impl Default for Castling {
+    fn default() -> Self { Castling(0u8) }
+}
 
 impl Castling {
     /// Both sides can still castle either way
-    pub const FULL: Castling = Castling(u4::new(0b1111));
+    pub const FULL: Castling = Castling(0b1111u8);
 
     /// Both sides cannot castle either way
-    pub const EMPTY: Castling = Castling(u4::new(0b0000));
+    pub const EMPTY: Castling = Castling(0b0000u8);
 
     /// Creates a new `Castling`
     pub fn new(v: u8) -> Castling {
-        Castling(u4::new(v))
+        Castling(v)
     }
 
-    /// Returns a flag representing the castling rights for one player 
-    /// and one side
-    pub fn flag(w: Whose, cs: CastlingSide) -> Castling {
-        if w == Whose::Ours {
-            if cs == CastlingSide::Kingside { 
-                return Castling(u4::new(0b1000));
-            } else { 
-                return Castling(u4::new(0b0100));
+    pub fn get(&mut self, w: Whose, cs: CastlingSide) -> bool {
+        let Castling(v) = *self;
+        match (w, cs) {
+            (Whose::Ours, CastlingSide::Kingside) => {
+                (v & 0b1000u8) == 0b1000u8
             }
-        } else { // w == Whose::Theirs
-            if cs == CastlingSide::Kingside { 
-                return Castling(u4::new(0b0010));
-            } else { 
-                return Castling(u4::new(0b0001));
+            (Whose::Ours, CastlingSide::Queenside) => {
+                (v & 0b0100u8) == 0b0100u8
+            }
+            (Whose::Theirs, CastlingSide::Kingside) => {
+                (v & 0b0010u8) == 0b0010u8
+            }
+            (Whose::Theirs, CastlingSide::Queenside) => {
+                (v & 0b0001u8) == 0b0001u8
             }
         }
     }
-}
 
-impl ops::BitOr for Castling {
-    type Output = Self;
-
-    fn bitor(self, rhs: Self) -> Self::Output {
-        Self(self.0 | rhs.0)
+    pub fn set(&mut self, w: Whose, cs: CastlingSide) -> () {
+        let Castling(v) = *self;
+        match (w, cs) {
+            (Whose::Ours, CastlingSide::Kingside) => {
+                *self = Castling(v | 0b1000u8);
+            }
+            (Whose::Ours, CastlingSide::Queenside) => {
+                *self = Castling(v | 0b0100u8);
+            }
+            (Whose::Theirs, CastlingSide::Kingside) => {
+                *self = Castling(v | 0b0010u8);
+            }
+            (Whose::Theirs, CastlingSide::Queenside) => {
+                *self = Castling(v | 0b0001u8);
+            }
+        }
     }
-}
 
-impl ops::BitAnd for Castling {
-    type Output = Self;
+    pub fn reset(&mut self, w: Whose, cs: CastlingSide) -> () {
+        let Castling(v) = *self;
+        match (w, cs) {
+            (Whose::Ours, CastlingSide::Kingside) => {
+                *self = Castling(v & 0b0111u8);
+            }
+            (Whose::Ours, CastlingSide::Queenside) => {
+                *self = Castling(v & 0b1011u8);
+            }
+            (Whose::Theirs, CastlingSide::Kingside) => {
+                *self = Castling(v & 0b1101u8);
+            }
+            (Whose::Theirs, CastlingSide::Queenside) => {
+                *self = Castling(v & 0b1110u8);
+            }
+        }
+    }
 
-    fn bitand(self, rhs: Self) -> Self::Output {
-        Self(self.0 & rhs.0)
+    /// Flips the castling flags such that Our flags become Theirs and Theirs
+    /// Ours
+    pub fn flip(&mut self) -> () {
+        let Castling(val) = *self;
+        let our_new = (val & 0b1100u8) >> 2;
+        let their_new = (val & 0b0011u8) << 2;
+        *self = Castling(our_new | their_new);
     }
 }
 
@@ -65,9 +96,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_flags() {
-        assert_eq!(Castling::flag(Whose::Ours, CastlingSide::Kingside), 
-                   Castling::new(0b1000));
+    fn test_get_set_reset() {
+        let mut c = Castling::new(0u8);
+        assert_eq!(c, Castling::default());
+        c.set(Whose::Ours, CastlingSide::Kingside);
+        assert_eq!(c, Castling(0b1000u8));
+        assert_eq!(c.get(Whose::Ours, CastlingSide::Kingside), true);
+        assert_eq!(c.get(Whose::Theirs, CastlingSide::Queenside), false);
+        c.reset(Whose::Ours, CastlingSide::Kingside);
+        assert_eq!(c.get(Whose::Ours, CastlingSide::Kingside), false);
     }
 
 }
